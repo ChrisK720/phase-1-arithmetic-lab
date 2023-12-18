eaa44a35fe98fa0083b8cd4e94cc174819850f98
Christopher Kirathe <ckirathe2005@gmail.com>
2023-12-18 11:18:59 +0300

    1st attempt at assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-// Write your code here
+const num3 = 16
+const num4 = 3
+function multiply(num1,num2){
+    const num2 = 31
+    const num1 = 2
+    return num1 * num2
+}
+function random(){
+    (Math.celi(Math.random() * 10)) + 1
+} 
+function mod(num1, num2){
+    const num1 = 19
+    const num2 = 5
+    return num1 % num2
+}
+
+function max(anarray){
+    const anArray = [17,1,5,8,9,20]
+    return Math.max(anArray)
+}
